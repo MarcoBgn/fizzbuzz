77468c302091048adb5e6e7362e5b6bfb8211629
MarcoBagnasco <marcobagnasco@ymail.com>
2016-01-11 17:40:24 +0000

    tests passing

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,3 @@
 def fizzbuzz(n)
-  n == 3 ? "fizz" : n
+  n % 3 == 0 && n % 5 == 0 ? "fizzbuzz" : (n % 3 == 0 ? "fizz" : (n % 5 == 0 ? "buzz" : n))
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,7 +1,19 @@
-require "fizzbuzz"
+require_relative "../lib/fizzbuzz"
 
 describe "fizzbuzz" do 
   it 'returns "fizz" when passed 3' do 
     expect(fizzbuzz(3)).to eq "fizz"
+  end
+  it 'returns "buzz" when passed 5' do 
+    expect(fizzbuzz(5)).to eq "buzz"
+  end
+  it 'returns "fizz" if a number is multiple of 3' do
+    expect(fizzbuzz(9)).to eq "fizz"
+  end
+  it 'returns "buzz" if a number is multiple of 5' do
+    expect(fizzbuzz(10)).to eq "buzz"
+  end
+  it 'returns "Fizzbuzz" if a number is both multiple of 3 and 5' do 
+    expect(fizzbuzz(15)).to eq "fizzbuzz"
   end
 end
